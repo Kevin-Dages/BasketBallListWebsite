0de5f84376075d16b32d0e8cd1830644610cd0c9
Kevin Dages <kevin.dages79@gmail.com>
2022-10-12 14:57:24 -0700

    This was a team project that me and my team of two others making a MVC App that you can create a basketball team!

---
+++ BasketBallWorkshopSpetacular/BasketBallWorkshopSpetacular/BasketballContext.cs
@@ -0,0 +1,28 @@
+﻿using Basketball_Workshop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basketball_Workshop
+{
+    public class BasketballContext : DbContext
+    {
+        public DbSet<Position>? Positions { get; set; }
+        public DbSet<PlayerPosition>? PlayerPositions { get; set; }
+        public DbSet<Player>? Players { get; set; }
+        public DbSet<Team>? Teams { get; set; }
+        public DbSet<Coach>? Coaches { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=BasketballDB;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team() { Id = 1, Name = "Cavs" },  
+                new Team() { Id = 2, Name = "Bulls"});
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BasketBallWorkshopSpetacular/BasketBallWorkshopSpetacular/Migrations/20221011215251_SeedDataTeams.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasketBallWorkshopSpetacular.Migrations
+{
+    public partial class SeedDataTeams : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Cavs" });
+
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Bulls" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
